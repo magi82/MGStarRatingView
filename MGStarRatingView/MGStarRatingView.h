89b4695cdb36f128c0cbf8075dfd91ddf24a627e
byungkook hwang <bkhwang82@gmail.com>
2017-05-26 18:46:06 +0900

    added the carthage project.

---
+++ MGStarRatingView/MGStarRatingView.h
@@ -0,0 +1,19 @@
+//
+//  MGStarRatingView.h
+//  MGStarRatingView
+//
+//  Created by 황병국 on 26/05/2017.
+//  Copyright © 2017 magi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MGStarRatingView.
+FOUNDATION_EXPORT double MGStarRatingViewVersionNumber;
+
+//! Project version string for MGStarRatingView.
+FOUNDATION_EXPORT const unsigned char MGStarRatingViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MGStarRatingView/PublicHeader.h>
+
+
