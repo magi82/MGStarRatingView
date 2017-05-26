796097830fc58655ca6011ce298538cb3f0dc395
byungkook hwang <bkhwang82@gmail.com>
2017-05-26 18:33:32 +0900

    added the library source file and the pod spec file.

---
+++ MGStarRatingView.podspec
@@ -0,0 +1,11 @@
+Pod::Spec.new do |s|
+  s.name             = 'MGStarRatingView'
+  s.version          = '0.1.0'
+  s.summary          = 'MGStarRatingView is a view for rating.'
+  s.homepage         = 'https://github.com/magi82/MGStarRatingView'
+  s.license          = { :type => 'MIT', :file => 'LICENSE' }
+  s.author           = { 'magi82' => 'bkhwang82@gmail.com' }
+  s.source           = { :git => 'https://github.com/magi82/MGStarRatingView.git', :tag => s.version.to_s }
+  s.ios.deployment_target = '8.0'
+  s.source_files = 'Sources/*.swift'
+end
---
+++ Sources/StarRatingView.swift
@@ -0,0 +1,328 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2017 ByungKook Hwang (https://magi82.github.io)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import UIKit
+
+public enum StarRatingType {
+  case rate
+  case half
+  case fill
+
+  static func type(by type: String) -> StarRatingType {
+    switch type {
+    case "rate":
+      return .rate
+    case "half":
+      return .half
+    case "fill":
+      return .fill
+    default:
+      return .rate
+    }
+  }
+}
+
+public protocol StarRatingDelegate {
+  func StarRatingValueChanged(value: CGFloat)
+}
+
+public struct StarRatingAttribute {
+  var type: StarRatingType = .rate
+  var point: CGFloat = 16
+  var spacing: CGFloat = 4
+  var emptyColor: UIColor = .lightGray
+  var fillColor: UIColor = .darkGray
+}
+
+@IBDesignable
+public class StarRatingView: UIView {
+  public var delegate: StarRatingDelegate?
+
+  @IBInspectable
+  public var type: StarRatingType = .rate {
+    didSet {
+      updateLocation(CGPoint(x: self.currentWidth, y: 0))
+      setNeedsDisplay()
+    }
+  }
+  @IBInspectable
+  internal var typeString: String = "rate" {
+    didSet {
+      self.type = StarRatingType.type(by: typeString)
+    }
+  }
+  @IBInspectable
+  public var current: CGFloat = 0 {
+    didSet {
+      self.currentWidth = getRateToWidth(self.current)
+      setNeedsDisplay()
+    }
+  }
+  @IBInspectable
+  public var max: Int = 0 {
+    didSet {
+      self.maxWidth = getRateToWidth(CGFloat(self.max))
+      setNeedsDisplay()
+    }
+  }
+  @IBInspectable
+  public var spacing: CGFloat = 0 {
+    didSet {
+      if self.spacing < 0 {
+        self.spacing = 0
+      }
+
+      self.currentWidth = getRateToWidth(self.current)
+      self.maxWidth = getRateToWidth(CGFloat(self.max))
+      self.emptyStar = makeStarImage(pt: self.point, spacing: self.spacing, color: self.emptyColor)
+      self.fillStar = makeStarImage(pt: self.point, spacing: self.spacing, color: self.fillColor)
+      setNeedsDisplay()
+    }
+  }
+  @IBInspectable
+  public var point: CGFloat = 0 {
+    didSet {
+      self.currentWidth = getRateToWidth(self.current)
+      self.maxWidth = getRateToWidth(CGFloat(self.max))
+      self.emptyStar = makeStarImage(pt: self.point, spacing: self.spacing, color: self.emptyColor)
+      self.fillStar = makeStarImage(pt: self.point, spacing: self.spacing, color: self.fillColor)
+      setNeedsDisplay()
+    }
+  }
+  @IBInspectable
+  public var emptyColor: UIColor = UIColor.lightGray {
+    didSet {
+      self.emptyStar = makeStarImage(pt: self.point, spacing: self.spacing, color: self.emptyColor)
+      setNeedsDisplay()
+    }
+  }
+  @IBInspectable
+  public var fillColor: UIColor = UIColor.black {
+    didSet {
+      self.fillStar = makeStarImage(pt: self.point, spacing: self.spacing, color: self.fillColor)
+      setNeedsDisplay()
+    }
+  }
+
+  private var currentWidth: CGFloat = 0
+  private var maxWidth: CGFloat = 0
+  private var emptyStar: UIImage?
+  private var fillStar: UIImage?
+
+  public override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+
+  public required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+  }
+
+  public func configure(_ attribute: StarRatingAttribute, current: CGFloat = 0, max: Int = 0) {
+    self.type = attribute.type
+    self.point = attribute.point
+    self.spacing = attribute.spacing
+    self.emptyColor = attribute.emptyColor
+    self.fillColor = attribute.fillColor
+
+    self.current = current
+    self.max = max
+
+    self.backgroundColor = .clear
+    self.frame.size = self.intrinsicContentSize
+  }
+
+  private func getRateToWidth(_ rate: CGFloat) -> CGFloat {
+    var width = self.point * CGFloat(rate)
+    width = width + CGFloat(ceil(rate) - 1) * self.spacing
+
+    return width
+  }
+
+  private func makeStar(_ size: CGFloat, color: UIColor) -> UIImage? {
+    let starSize: Double = Double(size)
+    let xCenter: Double = starSize * 0.5
+    let yCenter: Double = starSize * 0.5
+    let r: Double = starSize * 0.5
+    let flip: Double = -1.0
+
+    UIGraphicsBeginImageContextWithOptions(CGSize(width: starSize, height: starSize), false, 0)
+
+    guard let currentContext = UIGraphicsGetCurrentContext() else {
+      UIGraphicsEndImageContext()
+      return nil
+    }
+    currentContext.setFillColor(color.cgColor)
+    currentContext.setStrokeColor(color.cgColor)
+
+    currentContext.move(to: CGPoint(x: xCenter, y: r * flip + yCenter))
+    let theta: Double = 2.0 * .pi * (2.0 / 5.0)
+    for i in 1 ..< 5 {
+      let x: Double = Double(r * sin(Double(i) * theta))
+      let y: Double = Double(r * cos(Double(i) * theta))
+      currentContext.addLine(to: CGPoint(x: x + xCenter, y: y * flip + yCenter))
+    }
+
+    currentContext.closePath()
+    currentContext.fillPath()
+    let star = UIGraphicsGetImageFromCurrentImageContext()
+
+    UIGraphicsEndImageContext()
+
+    return star
+  }
+
+  private func makeStarImage(pt size: CGFloat, spacing: CGFloat, color: UIColor) -> UIImage? {
+    guard let starOrigin = self.makeStar(size, color: color) else { return nil }
+
+    var size = starOrigin.size
+    size.width = size.width + self.spacing
+    UIGraphicsBeginImageContextWithOptions(size, false, 0)
+    starOrigin.draw(at: CGPoint(x: 0, y: 0))
+    let starImage = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+
+    return starImage
+  }
+
+  public override func draw(_ rect: CGRect) {
+    super.draw(rect)
+
+    guard let emptyStar = self.emptyStar else { return }
+    emptyStar.drawAsPattern(in: CGRect(x: 0, y: 0, width: self.maxWidth, height: emptyStar.size.height))
+
+    if self.current > 0 {
+      guard let fillStar = self.fillStar else { return }
+      fillStar.drawAsPattern(in: CGRect(x: 0, y: 0, width: self.currentWidth, height: fillStar.size.height))
+    }
+  }
+
+  private func updateLocation(_ location: CGPoint) {
+    switch self.type {
+    case .rate:
+      var width = location.x < 0 ? 0 : location.x
+      width = width > self.maxWidth ? self.maxWidth : width
+      self.currentWidth = width
+
+      var count: CGFloat = 0
+      while width > 0 {
+        if width >= self.point {
+          count = count + 1
+          width = width - (self.point + self.spacing)
+        }
+        else {
+          count = count + (width / self.point)
+          width = width - (self.point + self.spacing)
+        }
+      }
+
+      self.current = count
+      delegate?.StarRatingValueChanged(value: count)
+      break
+
+    case .half:
+      var width = location.x < 0 ? 0 : location.x
+      width = width > self.maxWidth ? self.maxWidth : width
+
+      var count: CGFloat = 0
+      while width > 0 {
+        if width >= self.point {
+          count = count + 1
+          width = width - (self.point + self.spacing)
+        }
+        else {
+          count = count + (width / self.point)
+          width = width - (self.point + self.spacing)
+        }
+      }
+
+      self.current = floor(count)
+      let remainder = count - self.current
+      if remainder >= 0.5 {
+        self.current += 1
+      }
+      else if remainder > 0 {
+        self.current += 0.5
+      }
+
+      self.currentWidth = getRateToWidth(self.current)
+      delegate?.StarRatingValueChanged(value: self.current)
+      break
+
+    case .fill:
+      var width = location.x < 0 ? 0 : location.x
+      width = width > self.maxWidth ? self.maxWidth : width
+
+      var count: CGFloat = 0
+      while width > 0 {
+        if width >= self.point {
+          count = count + 1
+          width = width - (self.point + self.spacing)
+        }
+        else {
+          count = count + (width / self.point)
+          width = width - (self.point + self.spacing)
+        }
+      }
+
+      self.current = ceil(count)
+      self.currentWidth = getRateToWidth(self.current)
+      delegate?.StarRatingValueChanged(value: self.current)
+      break
+    }
+  }
+
+  public override var intrinsicContentSize: CGSize {
+    let count = CGFloat(self.max)
+    var width = self.point * count
+    width = width + CGFloat(count - 1) * self.spacing
+    return CGSize(width: width, height: self.point)
+  }
+
+  public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    super.touchesBegan(touches, with: event)
+
+    if let location = touches.first?.location(in: self) {
+      updateLocation(location)
+      setNeedsDisplay()
+    }
+  }
+
+  public override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    super.touchesMoved(touches, with: event)
+
+    if let location = touches.first?.location(in: self) {
+      updateLocation(location)
+      setNeedsDisplay()
+    }
+  }
+
+  public override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    super.touchesEnded(touches, with: event)
+
+    if let location = touches.first?.location(in: self) {
+      updateLocation(location)
+      setNeedsDisplay()
+    }
+  }
+}
+
+
